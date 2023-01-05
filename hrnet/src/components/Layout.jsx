1e8480008595d8aaca4eff7141551cc826d2769e
Andy Ramaroson <andyramaroson@gmail.com>
2023-01-05 10:44:55 +0100

    [enhancement] Add form scss and pages home with navigation

--- hrnet/src/components/Layout.jsx
+++ hrnet/src/components/Layout.jsx
@@ -1,16 +1,16 @@
-// import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Navbar from './Navbar/Navbar'
-// import Home from '../pages/Home/Home'
+import Home from '../pages/Home/Home'
 // import Employees from '../pages/Employees/Employees.jsx'
 
 function Layout() {
   return (
     <>
       <Navbar />
-      {/* <Routes>
+      <Routes>
         <Route path="/" element={<Home />} />
-        <Route exact path="/employees" element={<Employees />} />
-      </Routes> */}
+        {/* <Route exact path="/employees" element={<Employees />} /> */}
+      </Routes>
     </>
   )
 }
---
+++ hrnet/src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import Form from '../../components/Form/Form'
+import './Home.scss'
+
+function Home() {
+  return (
+    <main className="add-employee">
+      <section className="add-employee-content">
+        <h1>Add an employee</h1>
+        <Form />
+      </section>
+    </main>
+  )
+}
+export default Home
