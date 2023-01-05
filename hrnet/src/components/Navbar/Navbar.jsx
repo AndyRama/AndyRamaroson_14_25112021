fc4dc0281f2789413c1b34e3d11f3c422e974e51
Andy Ramaroson <andyramaroson@gmail.com>
2023-01-05 18:24:52 +0100

    [Enhancement](navbar) switch link depending on current location

--- hrnet/src/components/Navbar/Navbar.jsx
+++ hrnet/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useLocation, Link } from 'react-router-dom'
+
 import IconAdd from '../../components/Icons/IconAdd'
 import IconBrand from '../../components/Icons/IconBrand'
 import IconList from '../Icons/IconList'
@@ -23,17 +24,23 @@ function Navbar() {
         </div>
         <div className="nav-item nav-menu">
           <h1>HRnet Employees</h1>
-          <Link to="./Employees" className="nav-menu-choice">
-            <IconList className="nav-ico" alt="Health Wealth logo menu list" />
-            <span>Current</span>
-          </Link>
-          <Link to="/" className="nav-menu-choice">
-            <IconAdd
-              className="nav-ico"
-              alt="Health Wealth logo Add employee"
-            />
-            <span>Create</span>
-          </Link>
+          {path === '/' ? (
+            <Link to="./Employees" className="nav-menu-choice">
+              <IconList
+                className="nav-ico"
+                alt="Health Wealth logo menu list"
+              />
+              <span>Current</span>
+            </Link>
+          ) : (
+            <Link to="/" className="nav-menu-choice">
+              <IconAdd
+                className="nav-ico"
+                alt="Health Wealth logo Add employee"
+              />
+              <span>Create</span>
+            </Link>
+          )}
         </div>
       </nav>
       <hr className="nav-shadow" />
