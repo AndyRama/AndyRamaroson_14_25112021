d914102e49e829e02ca13fa69c5d4730f794b73b
Andy Ramaroson <andyramaroson@gmail.com>
2023-01-06 11:39:25 +0100

    [Enhancement] Update components TableColumns, clean code and add name dynamic for page Home | Employee

--- hrnet/src/pages/Employees/Employees.jsx
+++ hrnet/src/pages/Employees/Employees.jsx
@@ -1,10 +1,15 @@
 import Table from '../../components/Table/Table'
+import { useEffect } from 'react'
 import './Employees.scss'
 
 function Employees() {
+  useEffect(() => {
+    document.title = 'HRnet | Employees'
+  })
+
   return (
     <main className="employees-list">
-      <h1>Current Employes</h1>
+      <h2>Current Employes</h2>
       <Table />
     </main>
   )
--- hrnet/src/pages/Home/Home.jsx
+++ hrnet/src/pages/Home/Home.jsx
@@ -1,11 +1,16 @@
 import Form from '../../components/Form/Form'
+import { useEffect } from 'react'
 import './Home.scss'
 
 function Home() {
+  useEffect(() => {
+    document.title = 'HRnet | Home'
+  })
+
   return (
     <main className="add-employee">
       <section className="add-employee-content">
-        <h1>Add an employee</h1>
+        <h2>Add an employee</h2>
         <Form />
       </section>
     </main>
