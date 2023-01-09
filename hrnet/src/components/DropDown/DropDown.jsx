b99b7a722aa103e6509caa996d75c2f093cd7d0b
Andy Ramaroson <andyramaroson@gmail.com>
2023-01-09 17:14:19 +0100

    [enhancement](form) update input with datePicker and react-select(not work)

--- hrnet/src/components/DropDown/DropDown.jsx
+++ hrnet/src/components/DropDown/DropDown.jsx
@@ -1,25 +1,33 @@
-function Dropdown({ className, id, label, select, handleChange }) {
+import React from 'react'
+import Select from 'react-select'
+import dropDownData from './../../data/dropDownData.json'
+
+function Dropdown({ className, id, label, select, type, value, handleChange }) {
   return (
     <div className={`form-newEmployee--inputWrapper ${className}`}>
       <label htmlFor={id}>{label}</label>
-      <select
-        className="dropdownList"
-        id={id}
-        onChange={handleChange}
-        aria-required="true"
-        required
-      >
-        {select.map((item) => (
-          <option
-            title="dropdownOption"
-            type="text"
-            value={item.value}
-            key={item.abbrev}
-          >
-            {item.label}
-          </option>
-        ))}
-      </select>
+      {type === 'text' ? (
+        <Select type="state" options={dropDownData} onChange={handleChange} />
+      ) : (
+        <select
+          className="dropdownList"
+          id={id}
+          onChange={handleChange}
+          aria-required="true"
+          required
+        >
+          {select.map((item) => (
+            <option
+              title="dropdownOption"
+              type="text"
+              value={item.value}
+              key={item.abbrev}
+            >
+              {item.label}
+            </option>
+          ))}
+        </select>
+      )}
     </div>
   )
 }
--- hrnet/src/components/Input/Input.jsx
+++ hrnet/src/components/Input/Input.jsx
@@ -1,16 +1,23 @@
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
+
 function Input({ className, id, label, type, value, handleChange }) {
   return (
     <div className={`form-newEmployee--inputWrapper ${className}`}>
       <label htmlFor={id}>{label}</label>
-      <input
-        type={type}
-        id={id}
-        value={value}
-        onChange={handleChange}
-        autoComplete="off"
-        aria-required="true"
-        required
-      />
+      {type === 'date' ? (
+        <DatePicker selected={value} onChange={handleChange} />
+      ) : (
+        <input
+          type={type}
+          id={id}
+          value={value}
+          onChange={handleChange}
+          autoComplete="off"
+          aria-required="true"
+          required
+        />
+      )}
     </div>
   )
 }
